5122dd0b19b5bf8eaa2bdfc85d1a464ecfb0a60b
DESKTOP-MDPR1BV\mayuru <mariru10000>
2026-09-10 03:47:19 +0900

    メインゲーム、手札ロジックとか実装(未完)

---
+++ Team_4/Assets/Dekura/TurnManager.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class TurnManager : MonoBehaviour
+{
+    public enum TurnState
+    {
+        PlayerTurn,
+        EnemyTurn,
+        Changing
+    }
+
+    public static TurnManager Instance { get; private set; }
+    public TurnState NowTurn { get; private set; } = TurnState.PlayerTurn;
+    public event Action<TurnState> OnTurnChanged;          //ターン切り替わり時に通知
+
+    [SerializeField]private Button pturnEndButton;         //Pターン切り替えボタン
+    [SerializeField]private float turnChangeDiray = 2f;    //ターン切り替わりディレイ
+
+    private bool isTurnChanging = false;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        pturnEndButton.onClick.AddListener(TurnChange);
+    }
+
+    private void Update()
+    {
+        if (NowTurn == TurnState.EnemyTurn) TurnChange();
+    }
+
+    public void TurnChange()
+    {
+        if (isTurnChanging) return;
+        StartCoroutine(TurnChanfeRoutine());
+    }
+
+    private IEnumerator TurnChanfeRoutine()
+    {
+        isTurnChanging = true;
+
+        TurnState nextTurn = (NowTurn == TurnState.PlayerTurn) ? TurnState.EnemyTurn : TurnState.PlayerTurn;
+        turnAnnounse(nextTurn);
+        NowTurn = TurnState.Changing;
+        Debug.Log("TurnChanging...");
+
+        yield return new WaitForSeconds(turnChangeDiray);
+
+        NowTurn = nextTurn;
+        OnTurnChanged?.Invoke(NowTurn);
+        isTurnChanging = false;
+    }
+
+    private void turnAnnounse(TurnState turn)
+    {
+        switch(turn)
+        {
+            case TurnState.PlayerTurn:
+                Debug.Log("PlayerTurn");
+                break;
+
+            case TurnState.EnemyTurn:
+                Debug.Log("EnemyTurn");
+                break;
+        }
+    }
+}
